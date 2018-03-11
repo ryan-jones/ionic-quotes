ffc2eaf2f75907481f79f5add42ea6ac40cc44a2
Ryan <ryanjones@Ryans-MacBook-Pro.local>
2018-03-11 09:15:42 +0100

    fixed accidentally deleted variable

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote, Quotes } from '../../app/data/quotes';
+import { QuotesPage } from '../quotes/quotes';
 
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
 export class LibraryPage implements OnInit {
+  private quotesPage = QuotesPage;
   private collection: { category: string, quotes: Quote[], icon: string }[];
   constructor() {}
 
